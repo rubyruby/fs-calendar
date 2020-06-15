81bf5e480b5f12097f036753c758a86cae08418a
kanfet <byte.developer@gmail.com>
2020-06-15 18:10:33 +0500

    "bulma" added, login \ registration forms with bulma markup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def form_field_errors(model, field_name)
+    return unless model.errors.key?(field_name)
+
+    content_tag(:p, class: 'help is-danger') do
+      model.errors[field_name].join(", ")
+    end
+  end
+
 end
